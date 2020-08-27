ae3d63d07054969e39ca4079f730f2a14f883742
Israel Araujo <rodg.isra@gmail.com>
2020-08-27 01:22:09 -0300

    exercicio simples de tratamento de exceção e criação de tratamento personalizado

---
+++ Tratamentos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tratamentos.Entitites;
+using Tratamentos.Exceptions;
+
+namespace Tratamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1001, "Israel", 500.00, 250);
+            try
+            {
+                account.Withdraw(251);
+
+                Console.WriteLine(account.Balance);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
